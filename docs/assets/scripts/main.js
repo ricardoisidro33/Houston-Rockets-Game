2382b06aca0486e7ccc1e6316131d7b93f964d7d
Ricardo Isidro <ricardo.isidro33@gmail.com>
2022-10-18 11:45:46 +0100

    minor changes

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -1,8 +1,22 @@
 
 window.onload = () =>{
     document.getElementById('start-button').onclick =() => {
+        document.getElementById('start-button').style.display = "none";
+        document.getElementById('game1').style.display = "block";
+        document.getElementById('game2').style.display = "block";
+    }
+
+    document.getElementById('game1').onclick =() => {
+        document.getElementById('again-button').style.display = "block";
+        document.getElementById('game1').style.display = "none";
+        document.getElementById('game2').style.display = "none";
         startGame();
     }
+
+    document.getElementById('again-button').onclick =() => {
+        
+    }
+
     //function to start the game
     function startGame(){
         // create game and start game
@@ -10,4 +24,8 @@ window.onload = () =>{
         game.start();
     }
     
+
+
+    
+
 }
